d8d717dd6905c520e01da210f48e4ce5a87c4cc9
miaonster <miaonster17@gmail.com>
2021-04-08 23:49:08 +0800

    feat: add an option remove that you can specify array of glob patter to filter definition file path

--- README.md
+++ README.md
@@ -15,7 +15,34 @@ Typescript React Go To Definition Plugin
 
 ## Usage
 
-Instal VSCode extension
+Instal [VSCode extension](https://marketplace.visualstudio.com/items?itemName=miaonster.vscode-tsx-arrow-definition) or just use it as a typscript server plugin
+
+```
+npm i typescript-react-go-to-definition-plugin
+```
+
+```json
+// tsconfig.json
+{
+  "compilerOptions": {
+    "plugins": [
+      {
+        "name": "typescript-react-go-to-definition-plugin",
+        "remove": ["**/node_modules/**/*"]
+      }
+    ]
+  }
+}
+```
+
+### Options
+
+* `remove`
+
+  Default `["**/node_modules/@types/react/index.d.ts"]`
+
+  Array of glob pattern, see [minimatch](https://github.com/isaacs/minimatch) for more details.
+
 
 ## Purpose
 
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,28 +1,30 @@
-import * as ts from 'typescript/lib/tsserverlibrary';
+import * as ts from 'typescript/lib/tsserverlibrary'
+import * as minimatch from 'minimatch'
 
 export class ReactGoToDefinitionPlugin {
-    public constructor(
-        private readonly typescript: typeof ts
-    ) { }
+  public constructor(
+    private readonly typescript: typeof ts
+  ) { }
 
-    public create(info: ts.server.PluginCreateInfo): ts.LanguageService {
-        const proxy: ts.LanguageService = Object.create(null);
-        for (const k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
-          const x = info.languageService[k];
-          // @ts-ignore
-          proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args);
-        }
-
-        proxy.getDefinitionAndBoundSpan = (filename: string, position: number) => {
-          const prior = info.languageService.getDefinitionAndBoundSpan(filename, position);
-          // info.project.projectService.logger.info('xxx: ' + JSON.stringify(prior))
-          if (prior && prior.definitions && prior.definitions.length > 1) {
-            const definitions = prior?.definitions?.filter(x => !x.fileName.includes('@types/react/index.d.ts'))
-            return { ...prior, definitions }
-          }
-          return prior
-        }
-
-        return proxy;
+  public create(info: ts.server.PluginCreateInfo): ts.LanguageService {
+    const whatToRemove: string[] = info.config.remove || ['**/node_modules/@types/react/index.d.ts']
+    const proxy: ts.LanguageService = Object.create(null)
+    for (const k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
+      const x = info.languageService[k]
+      // @ts-ignore
+      proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args)
     }
+    proxy.getDefinitionAndBoundSpan = (filename: string, position: number) => {
+      const prior = info.languageService.getDefinitionAndBoundSpan(filename, position)
+      if (prior && prior.definitions && prior.definitions.length > 1) {
+        const definitions = prior.definitions.filter(x => {
+          return !whatToRemove.find(item => minimatch(x.fileName, item))
+        })
+        // info.project.projectService.logger.info('xxx: ' + JSON.stringify(whatToRemove))
+        return { ...prior, definitions }
+      }
+      return prior
+    }
+    return proxy
+  }
 }
